d6cda7b1b5165d0b5793c3a91ea2620306aa9ca3
Moulik-07 <moulikmachaiah724@gmail.com>
2025-03-13 16:08:27 +0530

    Update hello.cpp

--- main/hello.cpp
+++ main/hello.cpp
@@ -3,7 +3,7 @@
 using namespace std;
 
 int main(void) {
-  cout << "Hello, World\n";
+  cout << "Hello, World, this is Moulik- PES2UG22CS322\n";
   cout << "Hello, Jenkins\n";
   cout << "I have successfully built and run this  \n";
   return 0;
